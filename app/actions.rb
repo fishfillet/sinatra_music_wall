1ddb306100501436dd84021aaa50b4488ea6b7ce
fishfillet <daniel.susetyo@gmail.com>
2015-03-23 22:29:35 +0000

    update some css

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,7 @@
 # Homepage (Root path)
 get '/' do
-  erb :index
+  # erb :index
+  redirect '/songs'
 end
 
 get '/songs' do
@@ -8,25 +9,25 @@ get '/songs' do
   erb :'songs/index'
 end
 
-# get '/songs/new' do
-#   @songs = Song.new
-#   erb :'songs/new'
-# end
+get '/songs/new' do
+  @songs = Song.new
+  erb :'songs/new'
+end
 
 # get '/songs/:id' do
 #   @songs = Song.find params[:id]
 #   erb :'songs/show'
 # end
 
-# post '/songs' do
-#   @songs = Song.new(
-#     song_title:   params[:song_title],
-#     url: params[:url],
-#     author:  params[:author]
-#   )
-#   if @songs.save
-#     redirect '/songs'
-#   else
-#     erb :'songs/new'
-#   end
-#end 
+post '/songs' do
+  @songs = Song.new(
+    song_title:   params[:song_title],
+    url: params[:url],
+    author:  params[:author]
+  )
+  if @songs.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
+end 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
 
   validates :song_title, presence: true, length: { maximum: 40 }
-  validates :url, presence: true, length: { maximum: 140 }
+  # validates :url, presence: true, length: { maximum: 140 }
   validates :author, presence: true, length: { maximum: 25 }
   
 end
