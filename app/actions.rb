fe2fe7474cc535deba85ba910b7ebc132d10794a
fishfillet <daniel.susetyo@gmail.com>
2015-03-23 23:25:20 +0000

    add login, signup, logout functions

--- app/actions.rb
+++ app/actions.rb
@@ -1,8 +1,40 @@
 # Homepage (Root path)
 get '/' do
   # erb :index
+  redirect '/login'
+end
+
+get '/login' do
+  erb :'/login'
+end
+
+post '/login' do
+  params[:username]
+  params[:password]
+
+  users = User.where(username: params[:username]).where(password: params[:password])
+  if users.count > 0
+    user = users[0]
+    response.set_cookie("username", :value => params[:username], :path => "/", :expires => Time.now + 60*60*24*365*3)
+    redirect '/songs'
+  else
+    # login failed
+    redirect '/?message=Failed'
+  end
+end
+
+post '/signup' do
+  User.create(username: params[:username], password: params[:password])
   redirect '/songs'
 end
+
+get '/logout' do
+  response.set_cookie("username", :value => '', :path => "/", :expires => Time.now - 60*60*24*365*3)
+  redirect '/login'
+end
+
+
+#################################################################################
 
 get '/songs' do
   @songs = Song.all
